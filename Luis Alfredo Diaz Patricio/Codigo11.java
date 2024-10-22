c3116a1b3dfabeac4ffea68a96a52a989aa68764
Alfredo Diaz <patrik.17089@gmail.com>
2024-10-22 12:47:51 -0600

    Correccion

--- Luis Alfredo Diaz Patricio/Codigo11.java
+++ Luis Alfredo Diaz Patricio/Codigo11.java
@@ -1,3 +1,13 @@
-public class Codigo11 {
-    
+class Codigo11{
+    public static void main(String[] args) {
+        char[] a = {'H','o','l','a','M','u','n','d','o','o'};//Arreglo de diez letras
+iterar(a);//Se llama a la funcion
+    }
+    public static void iterar(char[] a) {//La funcion 
+        for (int i=0; i< a.length; i++){//La iteracion dentro de la funcion
+            System.out.println(a[i]);//El sout
+            
+        }
+        
+    }
 }
